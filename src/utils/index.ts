8b07fd6c3df301f266183c53b0ea014ae207d7a7
Shane Rogers <shane@repllabs.com>
2023-03-03 15:09:33 -0800

    reduce totals

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,176 @@
-export const utilExample = () => true
+// @ts-nocheck
+export const utilExample = () => true;
+
+export interface TokenUsageSummary {
+  apiKeyId: string;
+  model: string;
+  provider: string;
+  countTotal: number;
+  countDailyAvg: number;
+  countMonthlyAvg: number;
+  tokenTotal: number;
+  tokenDailyAvg: number;
+  tokenMonthlyAvg: number;
+}
+
+const RATE_TABLE = {
+  id: "3b41a695-3c09-49db-8d03-e37673c260f0",
+  openai: {
+    "text-davinci-003": {
+      transform_quantity: {
+        divide_by: 1000,
+        round: "up"
+      },
+      // $0.02
+      unit_amount_decimal: "2"
+    },
+    "text-curie-001": {
+      transform_quantity: {
+        divide_by: 1000,
+        round: "up"
+      },
+      // $0.002
+      unit_amount_decimal: "0.2"
+    },
+    "text-babbage-001": {
+      transform_quantity: {
+        divide_by: 1000,
+        round: "up"
+      },
+      // $0.0005
+      unit_amount_decimal: "0.05"
+    },
+    "text-ada-001": {
+      transform_quantity: {
+        divide_by: 1000,
+        round: "up"
+      },
+      // $0.0004
+      unit_amount_decimal: "0.04"
+    },
+    "fineTuned": {
+      ada: {
+        transform_quantity: {
+          divide_by: 1000,
+          round: "up"
+        },
+        // $0.0016
+        unit_amount_decimal: "0.016"
+      }
+    }
+  },
+  published: "2023-03-03T22:42:43.604Z"
+};
+
+export const breakdown = ({ usage }: { usage: TokenUsageSummary[] }) => {
+  const result = usage.reduce(
+    (memo, summary: TokenUsageSummary) => {
+      const { countTotal, tokenTotal, apiKeyId: id, model, provider } = summary;
+      if (!memo[id]) {
+        memo[id] = {
+          all: {
+            countTotal: 0,
+            tokenTotal: 0,
+            amountTotal: 0
+          }
+        };
+      }
+      if (!memo[id][model]) {
+        memo[id][model] = {
+          countTotal: 0,
+          tokenTotal: 0,
+          amountTotal: 0
+        };
+      }
+      memo.all.countTotal += countTotal;
+      memo[id].all.countTotal += countTotal;
+      memo[id][model].countTotal += countTotal;
+
+      memo.all.tokenTotal += tokenTotal;
+      memo[id].all.tokenTotal += tokenTotal;
+      memo[id][model].tokenTotal += tokenTotal;
+
+      return memo;
+    },
+    {
+      all: {
+        countTotal: 0,
+        tokenTotal: 0,
+        amountTotal: 0
+      }
+    }
+  );
+
+  for (let index = 0; index < usage.length; index++) {
+    const summary = usage[index];
+    const { apiKeyId: id, model, provider } = summary;
+    const modelRate = RATE_TABLE[provider][model];
+    if (modelRate) {
+      const modelTokens = result[id][model].tokenTotal;
+      const unitCount = modelTokens / modelRate.transform_quantity.divide_by;
+      const unitRate = parseFloat(modelRate.unit_amount_decimal);
+
+      const amount = unitCount * unitRate;
+      console.log(
+        `------- ${model} (${unitCount} * ${unitRate}) amount:`,
+        amount
+      );
+      console.log("\n");
+
+      result.all.amountTotal += amount;
+      result[id].all.amountTotal += amount;
+      result[id][model].amountTotal += amount;
+    } else {
+      console.warn(`No rate for provider:${provider} model:${model}`);
+    }
+  }
+
+  return result;
+};
+
+// export const breakdown = ({ usage }: { usage: TokenUsageSummary[] }) => {
+//   const result = usage.reduce(
+//     (memo, summary) => {
+//       if (!memo[summary.apiKeyId]) {
+//         memo[summary.apiKeyId] = {
+//           all: {
+//             count: 0
+//           }
+//         };
+//       }
+
+//       return memo;
+//     },
+//     {
+//       all: {
+//         count: 0
+//       }
+//     } as {
+//       [key: string]:
+//         | {
+//             count: number;
+//           }
+//         | {
+//             [key: string]:
+//               | number
+//               | {
+//                   [key: string]: {
+//                     count: number;
+//                   };
+//                 };
+//             count: number;
+//           };
+//       all: {
+//         [key: string]:
+//           | number
+//           | {
+//               [key: string]: {
+//                 count: number;
+//               };
+//             };
+//         count: number;
+//       };
+//     }
+//   );
+//   return result;
+// };
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,12 +1,110 @@
 import * as chai from "chai";
-import { utilExample } from "@/utils";
+import { utilExample, breakdown } from "@/utils";
 const { assert } = chai;
 
 describe("src/utils", () => {
   describe("utilExample", () => {
     it("example test", () => {
-      const result = utilExample()
+      const result = utilExample();
       assert.isTrue(result);
+    });
+  });
+
+  describe.only("breakdown", () => {
+    it("example test", () => {
+      const result = breakdown({
+        usage: [
+          {
+            apiKeyId: "apiKey1",
+            model: "text-davinci-003",
+            provider: "openai",
+            countTotal: 2,
+            countDailyAvg: 0,
+            countMonthlyAvg: 0,
+            tokenTotal: 100000,
+            tokenDailyAvg: 0,
+            tokenMonthlyAvg: 0
+          },
+          {
+            apiKeyId: "apiKey1",
+            model: "text-curie-001",
+            provider: "openai",
+            countTotal: 4,
+            countDailyAvg: 0,
+            countMonthlyAvg: 0,
+            tokenTotal: 200000,
+            tokenDailyAvg: 0,
+            tokenMonthlyAvg: 0
+          },
+          {
+            apiKeyId: "apiKey2",
+            model: "text-davinci-003",
+            provider: "openai",
+            countTotal: 3,
+            countDailyAvg: 0,
+            countMonthlyAvg: 0,
+            tokenTotal: 150000,
+            tokenDailyAvg: 0,
+            tokenMonthlyAvg: 0
+          },
+          {
+            apiKeyId: "apiKey2",
+            model: "text-curie-001",
+            provider: "openai",
+            countTotal: 1,
+            countDailyAvg: 0,
+            countMonthlyAvg: 0,
+            tokenTotal: 50000,
+            tokenDailyAvg: 0,
+            tokenMonthlyAvg: 0
+          }
+        ]
+      });
+
+      // console.log(`---- result`, JSON.stringify(result, null, 2));
+
+      const target = {
+        all: {
+          countTotal: 6 + 4,
+          tokenTotal: 300000 + 200000,
+          amountTotal: 240 + 310
+        },
+        apiKey1: {
+          "all": {
+            countTotal: 6,
+            tokenTotal: 300000,
+            amountTotal: 240
+          },
+          "text-davinci-003": {
+            countTotal: 2,
+            tokenTotal: 100000,
+            amountTotal: 200
+          },
+          "text-curie-001": {
+            countTotal: 4,
+            tokenTotal: 200000,
+            amountTotal: 40
+          }
+        },
+        apiKey2: {
+          "all": {
+            countTotal: 4,
+            tokenTotal: 200000,
+            amountTotal: 310
+          },
+          "text-davinci-003": {
+            countTotal: 3,
+            tokenTotal: 150000,
+            amountTotal: 300
+          },
+          "text-curie-001": {
+            countTotal: 1,
+            tokenTotal: 50000,
+            amountTotal: 10
+          }
+        }
+      };
+      assert.deepEqual(result, target);
     });
   });
 });
